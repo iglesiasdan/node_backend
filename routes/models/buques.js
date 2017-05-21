c911bb101a75b8b1fe221db44c08fba23fdbfd65
Daniel Iglesias <iglesias.dan@gmail.com>
2017-05-20 21:14:07 -0400

    fix buque

--- routes/models/buques.js
+++ routes/models/buques.js
@@ -26,8 +26,8 @@ buqueModel.insert=function(input,callback){
 				Capacidad_tanques:input.capacidad_tanques,
 				Vol_total:input.vol_total,
 				Fotos:input.fotos
-			}
-			console.log(buque);
+			};
+			console.log(input);
 		connection.query('INSERT INTO buque SET ?',buque,function(err,rows,fields){
 			callback({'Error':false,'id':rows.insertId,'message':'Registro insertado exitosamente'});
 		})
