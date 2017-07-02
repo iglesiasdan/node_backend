760cfbc8ab35d36d8e3c1e8968b83a96203cbb1a
Daniel Iglesias <iglesias.dan@gmail.com>
2017-07-02 01:39:26 -0400

    abanderamiento bug

--- routes/models/arribos.js
+++ routes/models/arribos.js
@@ -14,7 +14,7 @@ arriboModel.get=function(callback){
 	});
 }
 arriboModel.getFrom=function(id,callback){
-	connection.query(`SELECT ID_arribo,Fecha_arribo,Observaciones,b.Abanderamiento,Numero_IMO,arr.ID_agencia,arr.ID_puerto,Nombre_puerto,arr.ID_buque,Nombre_buque,ag.Nombre_agencia
+	connection.query(`SELECT ID_arribo,Fecha_arribo,Observaciones,Abanderamiento,Numero_IMO,arr.ID_agencia,arr.ID_puerto,Nombre_puerto,arr.ID_buque,Nombre_buque,ag.Nombre_agencia
 		FROM arribo AS arr,agencia AS ag, puerto AS p, buque AS b WHERE ag.ID_agencia=arr.ID_agencia AND p.ID_puerto=arr.ID_puerto AND b.ID_buque=arr.ID_buque AND ID_arribo>?
 		ORDER BY Fecha_arribo desc`,[id],function(err, rows, fields) {
 	  if (err) {
