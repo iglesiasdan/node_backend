a5022259e661648eddfa16e8dedca9505d209c9e
Ocumo Web <ocumoweb@gmail.com>
2017-07-05 09:42:25 -0400

    estadisticas SAO y Node

--- routes/estadisticas.js
+++ routes/estadisticas.js
@@ -8,16 +8,23 @@ var bodyParser=require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}));
 
-router.get('/estabuques/:id',function(req,res){
+router.get('/estabuques/:id/:f1/:f2',function(req,res){
 
-	estadisticas.get(req.params.id,function(data){
+	estadisticas.get(req.params.id,req.params.f1,req.params.f2,function(data){
 		res.setHeader('Content-Type', 'application/json');
     	res.send(JSON.stringify(data));
 	});
 });
 
-router.get('/:id',function(req,res){
-	estadisticas.show(req.params.id,function(data){
+router.get('/:id/:f1/:f2',function(req,res){
+	estadisticas.show(req.params.id,req.params.f1,req.params.f2,function(data){
+		res.setHeader('Content-Type', 'application/json');
+    	res.send(JSON.stringify(data));
+	});
+});
+
+router.get('/detalle/:id/:f1/:f2/:idp',function(req,res){
+	estadisticas.detalles(req.params.id,req.params.f1,req.params.f2,req.params.idp,function(data){
 		res.setHeader('Content-Type', 'application/json');
     	res.send(JSON.stringify(data));
 	});
--- routes/models/estadisticas.js
+++ routes/models/estadisticas.js
@@ -3,16 +3,24 @@ var connection=require('./connection');
 var bcrypt=require('bcrypt-nodejs');
 var estadisticasModel={};
 
-estadisticasModel.get=function(id,callback){
-	connection.query('SELECT  AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_buque, buque.Nombre_buque FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND estudio.ID_arribo=arribo.ID_arribo AND arribo.ID_buque=buque.ID_buque AND buque.ID_buque='+id, function(err, rows, fields) {
+estadisticasModel.get=function(id,f1,f2,callback){
+	connection.query('SELECT  AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_buque, buque.Nombre_buque FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND estudio.ID_arribo=arribo.ID_arribo AND arribo.ID_buque=buque.ID_buque AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND buque.ID_buque='+id, function(err, rows, fields) {
 	  if (err) throw err;
 	  callback(rows);
 	});
 }
 
 // Para promedios de puertos
-estadisticasModel.show=function(id,callback){
-	connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_puerto,puerto.Nombre_puerto FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND arribo.ID_buque=buque.ID_buque AND estudio.ID_arribo=arribo.ID_arribo AND puerto.ID_puerto='+id, function(err, rows, fields) {
+estadisticasModel.show=function(id,f1,f2,callback){
+	connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_puerto,puerto.Nombre_puerto FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND arribo.ID_buque=buque.ID_buque AND estudio.ID_arribo=arribo.ID_arribo AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND puerto.ID_puerto='+id, function(err, rows, fields) {
+	  if (err) throw err;
+	  callback(rows);
+	});
+}
+
+estadisticasModel.detalles=function(id,f1,f2,idp,callback){
+	//connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_puerto,puerto.Nombre_puerto FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND arribo.ID_buque=buque.ID_buque AND estudio.ID_arribo=arribo.ID_arribo AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND puerto.ID_puerto='+id+'AND puerto.ID_puerto='+idp, function(err, rows, fields) {
+	connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_buque, buque.Nombre_buque FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND estudio.ID_arribo=arribo.ID_arribo AND arribo.ID_buque=buque.ID_buque AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND buque.ID_buque='+id+' AND puerto.ID_puerto='+idp, function(err, rows, fields) {
 	  if (err) throw err;
 	  callback(rows);
 	});
