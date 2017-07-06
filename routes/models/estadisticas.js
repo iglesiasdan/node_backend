6a5ace252693fbda78bb6e450f00bec9b5fb8ebd
Ocumo Web <ocumoweb@gmail.com>
2017-07-06 10:49:37 -0400

    Node estadisticas volumen total

--- routes/models/estadisticas.js
+++ routes/models/estadisticas.js
@@ -4,7 +4,7 @@ var bcrypt=require('bcrypt-nodejs');
 var estadisticasModel={};
 
 estadisticasModel.get=function(id,f1,f2,callback){
-	connection.query('SELECT  AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_buque, buque.Nombre_buque FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND estudio.ID_arribo=arribo.ID_arribo AND arribo.ID_buque=buque.ID_buque AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND buque.ID_buque='+id, function(err, rows, fields) {
+	connection.query('SELECT  AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_buque, buque.Nombre_buque , SUM(arribo.Volumen_total) AS Voltotal FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND estudio.ID_arribo=arribo.ID_arribo AND arribo.ID_buque=buque.ID_buque AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND buque.ID_buque='+id, function(err, rows, fields) {
 	  if (err) throw err;
 	  callback(rows);
 	});
@@ -12,7 +12,7 @@ estadisticasModel.get=function(id,f1,f2,callback){
 
 // Para promedios de puertos
 estadisticasModel.show=function(id,f1,f2,callback){
-	connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_puerto,puerto.Nombre_puerto FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND arribo.ID_buque=buque.ID_buque AND estudio.ID_arribo=arribo.ID_arribo AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND puerto.ID_puerto='+id, function(err, rows, fields) {
+	connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_puerto,puerto.Nombre_puerto, SUM(arribo.Volumen_total) AS Voltotal FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND arribo.ID_buque=buque.ID_buque AND estudio.ID_arribo=arribo.ID_arribo AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND puerto.ID_puerto='+id, function(err, rows, fields) {
 	  if (err) throw err;
 	  callback(rows);
 	});
@@ -20,7 +20,7 @@ estadisticasModel.show=function(id,f1,f2,callback){
 
 estadisticasModel.detalles=function(id,f1,f2,idp,callback){
 	//connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_puerto,puerto.Nombre_puerto FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND arribo.ID_buque=buque.ID_buque AND estudio.ID_arribo=arribo.ID_arribo AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND puerto.ID_puerto='+id+'AND puerto.ID_puerto='+idp, function(err, rows, fields) {
-	connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_buque, buque.Nombre_buque FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND estudio.ID_arribo=arribo.ID_arribo AND arribo.ID_buque=buque.ID_buque AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND buque.ID_buque='+id+' AND puerto.ID_puerto='+idp, function(err, rows, fields) {
+	connection.query('SELECT AVG(estudio.Salinidad) AS salinidad, AVG(estudio.Temperatura) AS temperatura, AVG(estudio.Conductividad) AS conductividad, AVG(estudio.Ph) AS ph, arribo.ID_buque, buque.Nombre_buque, SUM(arribo.Volumen_total) AS Voltotal FROM arribo, puerto, estudio, buque WHERE arribo.ID_puerto=puerto.ID_puerto AND estudio.ID_arribo=arribo.ID_arribo AND arribo.ID_buque=buque.ID_buque AND arribo.Fecha_arribo BETWEEN "'+f1+'" AND "'+f2+'" AND buque.ID_buque='+id+' AND puerto.ID_puerto='+idp, function(err, rows, fields) {
 	  if (err) throw err;
 	  callback(rows);
 	});
